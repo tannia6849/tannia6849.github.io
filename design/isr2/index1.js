e8995cec539b7cee2b1cec68cce6beffdf63f70e
tannia6849 <54927863+tannia6849@users.noreply.github.com>
2020-06-27 23:25:31 +0400

    Create index1.js

---
+++ design/isr2/index1.js
@@ -0,0 +1,11 @@
+const a1 = 'Bye';
+const a2 = 'Hello';
+let bst = 'bye'
+if (bst == 'Hello'){
+    console.log('Yes');
+}else{
+    console.log('No');
+    bst = 'Javascript';
+    console.log(bst);
+    console.log('Yes');
+}
