858f8d85a9926cbabe2209c3a03a3c8646a28af9
tannia6849 <54927863+tannia6849@users.noreply.github.com>
2021-11-30 19:26:28 +0300

    Add files via upload

---
+++ programming workshop/4 task/WebApplicationTest/WebApplicationTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationTest.Controllers
+{
+    [ApiController]
+    [Route("test")]
+
+    public class WeatherForecastController : Controller
+    {
+        [HttpPost]
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("index")]
+
+        public async Task<IActionResult> Index()
+        {
+            return this.Json(new { results = "Hello World!" });
+        }
+
+        [HttpPost]
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("hello_world")]
+
+        public async Task<IActionResult> Test()
+        {
+            MyClassResponse res = new MyClassResponse();
+            res.Success = "success";
+            res.Result = "Hello World!!";
+            res.Version = "1.0";
+            return this.Json(res);
+        }
+    }
+}
